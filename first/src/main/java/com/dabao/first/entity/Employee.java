f763916de17067cea3267161ac728a6405b4cc66
xiaodabao <dabaomonitor@163.com>
2020-08-18 16:16:07 +0800

    1

---
+++ first/src/main/java/com/dabao/first/entity/Employee.java
@@ -0,0 +1,17 @@
+package com.dabao.first.entity;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private int age;
+    private boolean admin;
+    private int deptId;
+}
